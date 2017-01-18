69cd74244050ad633cabaec41c983c31f350f34e
jagretz <jagretz@gmail.com>
2017-01-17 20:41:13 -0500

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
 /* eslint-disable no-new*/
-require([
-    'backbone',
-    'views/app.view',
-    'routers/router'
-], function (Backbone, AppView, Router) {
-    'use strict';
+// require([
+//     'backbone',
+//     'views/app.view',
+//     'routers/router'
+// ], function (Backbone, AppView, Router) {
+//     'use strict';
 
-    // Initialize routing and start tracing browser history
-    new Router();
-    Backbone.history.start();
+//     // Initialize routing and start tracing browser history
+//     new Router();
+//     Backbone.history.start();
 
-    // Initialize the application view
-    new AppView();
-});
-require.config({
-    // deps: '../app',
-    // sets up custom paths to common libraries, plugins, modules.
-    paths: {
-        'underscore': 'lib/underscore.min',
-        'jquery': 'lib/jquery.slim.min',
-        'backbone': 'lib/backbone.min',
-        'backbone-nested': 'lib/backbone-nested',
-        'backbone-localstorage': 'lib/backbone.localstorage',
-        'handlebars': 'lib/handlebars.min',
-        'text': 'lib/text'
-    },
-    // ! refer to custom path.
-    shim: {
-        'underscore': {
-            exports: '_'
-        },
-        'backbone': {
-            deps: ['jquery', 'underscore'],
-            exports: 'Backbone'
-        },
-        'localstorage': {
-            deps: ['backbone']
-        },
-        'backbone-nested': {
-            deps: ['backbone']
-        }
-    }
-});
+//     // Initialize the application view
+//     new AppView();
+// });
+// require.config({
+//     // deps: '../app',
+//     // sets up custom paths to common libraries, plugins, modules.
+//     paths: {
+//         'underscore': 'lib/underscore.min',
+//         'jquery': 'lib/jquery.slim.min',
+//         'backbone': 'lib/backbone.min',
+//         'backbone-nested': 'lib/backbone-nested',
+//         'backbone-localstorage': 'lib/backbone.localstorage',
+//         'handlebars': 'lib/handlebars.min',
+//         'text': 'lib/text'
+//     },
+//     // ! refer to custom path.
+//     shim: {
+//         'underscore': {
+//             exports: '_'
+//         },
+//         'backbone': {
+//             deps: ['jquery', 'underscore'],
+//             exports: 'Backbone'
+//         },
+//         'localstorage': {
+//             deps: ['backbone']
+//         },
+//         'backbone-nested': {
+//             deps: ['backbone']
+//         }
+//     }
+// });
