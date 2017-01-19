1b5b575dd079e02f983715c1ef1fae017dccc823
jagretz <jagretz@gmail.com>
2017-01-18 20:27:40 -0500

    updates to require configuration

--- js/config/require.config.js
+++ js/config/require.config.js
@@ -1,45 +1,47 @@
 'use strict'; // eslint-disable-line
 
-// require.config({
-//     // deps: '../app',
-//     // sets up custom paths to common libraries, plugins, modules.
-//     paths: {
-//         'underscore': '../lib/udnerscore',
-//         'jquery': '../lib/jquery.slim.min',
-//         'backbone': '../lib/backbone.min',
-//         'backbone-nested': '../lib/backbone-nested',
-//         'backbone-localstorage': 'js/lib/backbone.localstorage',
-//         'handlebars': '../lib/handlebars.min',
-//         'text': '../lib/text'
-//     },
-//     // ! refer to custom path.
-//     shim: {
-//         'underscore': {
-//             exports: '_'
-//         },
-//         'backbone': {
-//             deps: ['jquery', 'underscore'],
-//             exports: 'Backbone'
-//         },
-//         'localstorage': {
-//             deps: ['backbone']
-//         },
-//         'backbone-nested': {
-//             deps: ['backbone']
-//         }
-//     }
-// });
-// require([
-//     'backbone',
-//     'views/app.view',
-//     'routers/router'
-// ], function (Backbone, AppView, Router) {
-//     'use strict';
+require.config({
+    // deps: 'app',
+    baseUrl: 'js',
+    // sets up custom paths to common libraries, plugins, modules.
+    paths: {
+        'underscore': 'lib/underscore.min',
+        'jquery': 'lib/jquery.slim.min',
+        'backbone': 'lib/backbone.min',
+        'backbone-nested': 'lib/backbone-nested',
+        'backbone-localstorage': 'lib/backbone.localstorage',
+        'handlebars': 'lib/handlebars.min',
+        'text': 'lib/text'
+    },
+    // ! refer to custom path.
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'localstorage': {
+            deps: ['backbone']
+        },
+        'backbone-nested': {
+            deps: ['backbone']
+        }
+    }
+});
 
-//     // Initialize routing and start tracing browser history
-//     new Router();
-//     Backbone.history.start();
+require([
+    'backbone',
+    'views/app.view',
+    'routers/router'
+], function (Backbone, AppView, Router) {
+    'use strict';
 
-//     // Initialize the application view
-//     new AppView();
-// });
+    // Initialize routing and start tracing browser history
+    new Router();
+    Backbone.history.start();
+
+    // Initialize the application view
+    new AppView();
+});
